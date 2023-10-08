0d606d94ed426a4f7ff69dbda7dbd428895470bb
frankmolcas92 <37858279+Aka-DoctorCode@users.noreply.github.com>
2023-10-08 19:10:38 -0400

    1.0.1

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 const express = require ('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { response } = require('express');
 
 // app
 const app = express();
@@ -105,21 +106,147 @@ const farmaco = new mongoose.Schema({
         type: String,
         default: "No requerido ajuste"
     },
-
 });
 
 // modelo
 const Farmaco = mongoose.model('Farmaco', farmaco);
 
 // rutas
-app.get("/getfarmacos", async (req, res)=>{
+app.get("/farmacos", async (req, res)=>{
     const farmacosEncontrados = await Farmaco.find();
     res.status(200).json(farmacosEncontrados);
-})
 
+    if (farmacosEncontrados != null) {
+        response.status(200).json(farmacosEncontrados);
+    } else {
+        response.status(404).json({
+            "error": "No se encontraron farmacos",
+            "solucion": "Intenta con otro"
+        });
+    }
+});
 
+app.get("/farmaco/:nombre", async (req, res)=>{
+    const {nombre} = req.params;
+    const farmacoEncontrado = await Farmaco.findOne({"nombre": nombre});
 
+    if (farmacoEncontrado != null) {
+        response.status(200).json(farmacoEncontrado);
+    }else{
+        response.status(404).json({
+            "error": "No se encontro el farmaco",
+            "solucion": "Intenta con otro"
+        });
+    }
+});
 
+app.get("/farmaco/:familia", async (req, res)=>{
+    const {familia} = req.params;
+    const farmacoEncontrado = await Farmaco.findAll({"familia": familia});
+
+    if (farmacoEncontrado != null) {
+        response.status(200).json(farmacoEncontrado);
+    }else{
+        response.status(404).json({
+            "error": "No se encontro el farmaco",
+            "solucion": "Intenta con otro"
+        });
+    }
+});
+
+app.post("/agregarFarmaco", async (req, res)=>{
+    const {id, nombre, familia, mecanismoDeAccion, indicacion, presentaciones, viaAdministracion, dosisAdulto, dosisMaxAdult, dosisPediatrica, dosisMaxPedia, riesgo, contraindicaciones, ajusteRenal, ajusteHepatico} = req.body;
+
+    try {
+        const nuevoFarmaco = new Farmaco({id, nombre, familia, mecanismoDeAccion, indicacion, presentaciones, viaAdministracion, dosisAdulto, dosisMaxAdult, dosisPediatrica, dosisMaxPedia, riesgo, contraindicaciones, ajusteRenal, ajusteHepatico});
+
+        await nuevoFarmaco.save();
+
+        res.status(200).json({
+            "Se agrego el farmaco": nuevoFarmaco.nombre
+        });
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            "error": error,
+            "No se pudo agregar el farmaco": "Intenta con otro",
+            "solucion": "Intenta con otro"
+        });
+    }
+});
+
+app.put("/farmaco/:nombre", async (req, res)=>{
+    const {nombreFarmaco} = req.params;
+
+    const {id, nombre, familia, mecanismoDeAccion, indicacion, presentaciones, viaAdministracion, dosisAdulto, dosisMaxAdult, dosisPediatrica, dosisMaxPedia, riesgo, contraindicaciones, ajusteRenal, ajusteHepatico} = req.body;
+
+    try {
+        const farmacoModificado = await Farmaco.findOneAndUpdate(
+            {"nombre": nombreFarmaco},
+            {id, nombre, familia, mecanismoDeAccion, indicacion, presentaciones, viaAdministracion, dosisAdulto, dosisMaxAdult, dosisPediatrica, dosisMaxPedia, riesgo, contraindicaciones, ajusteRenal, ajusteHepatico}
+        );
+
+        res.status(200).json({
+            "Se actualizo el farmaco": farmacoModificado,
+            "NuevosDatos": {
+                "nombre": nombre,
+                "familia": familia,
+                "mecanismoDeAccion": mecanismoDeAccion,
+                "indicacion": indicacion,
+                "presentaciones": presentaciones,
+                "viaAdministracion": viaAdministracion,
+                "dosisAdulto": dosisAdulto,
+                "dosisMaxAdult": dosisMaxAdult,
+                "dosisPediatrica": dosisPediatrica,
+                "dosisMaxPedia": dosisMaxPedia,
+                "riesgo": riesgo,
+                "contraindicaciones": contraindicaciones,
+                "ajusteRenal": ajusteRenal,
+                "ajusteHepatico": ajusteHepatico
+            },
+            "datosAnteriores" : farmacoModificado
+        });
+    }catch(error){
+        res.status(400).json({
+            "error": error,
+            "Solucion": "Asegurate que el farmaco este bien escrito, o puede que este no exista aún en la base de datos"
+        });
+    }
+});
+
+app.patch("/farmaco/:nombre", async (req, res) =>{
+    const nuevosDatos = req.body;
+
+    try {
+        await Farmaco.updateOne(
+            {"nombre": req.params.nombre},
+            {$set: nuevosDatos}
+        );
+        res.status(200).json({
+            "Se actualizo el farmaco" : `${nuevosDatos.nombre}`,
+            "NuevosDatos": {nuevosDatos}
+        })
+    } catch (error) {
+        res.status(400).json({
+            "error": error,
+            "Solución": "Asegurate que el farmaco este bien escrito, o puede que este no exista en la base de datos"
+        });
+    }
+});
+
+app.delete("/farmaco/:nombre", async (req, res)=>{
+    const farmacoAEliminar = await Farmaco.findOneAndDelete({"nombre": req.params.nombre});
+
+    farmacoAEliminar != null
+        ? res.status(200).json({
+            "respuesta": `Se elimino el farmaco ${farmacoAEliminar.nombre}`,
+            "farmaco": farmacoAEliminar
+        })
+        : res.status(404).json({
+            "respuesta": "No se encontro el farmaco",
+            "farmaco": farmacoAEliminar
+        });
+});
 
 
 // escuchar el puerto
@@ -127,5 +254,3 @@ app.listen(7000, () => {
     console.log("Servidor escuchando en el puerto 7000");
     console.log("http://localhost:7000");
 })
-
-// 
